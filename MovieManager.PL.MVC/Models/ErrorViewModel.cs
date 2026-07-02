7046e759faabc079af21e2fd5804aab7f29a4b8d
Rhaastrake <garofalomichele2001@gmail.com>
2026-07-02 14:37:08 +0200

    First commit

---
+++ MovieManager.PL.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MovieManager.PL.MVC.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
